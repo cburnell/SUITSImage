cce96798c37c23cc012f8829cfe27c3f01a3479a
mustelidae1 <othm@live.com>
2019-02-09 13:07:38 -0700

    Added magical statements to fix issue

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+// NOTE: Replaced these import statements with the one below
+//       when the server unexpectedly stopped working.
+//       No idea why they stopped working but we're going with it.
+
+//const express = require('express');
+//const app = express();
+//const http = require('http').Server(app);
+//const io = require('socket.io')(http);
+//const port = process.env.PORT || 3000;
+
+var express = require("express");
+var http = require("http");
+var app = express();
+var server = http.createServer(app).listen(3000);
+var io = require("socket.io")(server);
+var fs = require('fs');
 
 app.use(express.static(__dirname + '/'));
 
 function onConnection(socket){
+  console.log('Connection made');
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
   socket.on('picture', (data) => socket.broadcast.emit('picture', data));
-  socket.on('pictureFromHolo', function (data) { 
+  socket.on('pictureFromHolo', function (data) {
 				socket.broadcast.emit('pictureFromHolo', data);
 				console.log("from hololens");
 				}
@@ -20,4 +32,4 @@ function onConnection(socket){
 
 io.on('connection', onConnection);
 
-http.listen(port, () => console.log('listening on port ' + port));
+//http.listen(port, () => console.log('listening on port ' + port));
